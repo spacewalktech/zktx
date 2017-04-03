83df680b98d2b4de25c82704f60d4c2381feb42c
sailLiao <1065500519@qq.com>
2017-04-03 10:03:52 +0800

    test

--- web/src/main/java/com/zktx/platform/controller/ImportTablesController.java
+++ web/src/main/java/com/zktx/platform/controller/ImportTablesController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.zktx.platform.entity.tb.ImportTablesPo;
 import com.zktx.platform.entity.tb.ImportTablesWithBLOBs;
 import com.zktx.platform.service.importtable.ImportTableService;
