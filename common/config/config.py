c8be31129f645a6feb4591409ca46f2d9a302070
sailLiao <1065500519@qq.com>
2017-04-16 15:29:37 +0800

    filescan

--- common/config/config.py
+++ common/config/config.py
@@ -25,13 +25,11 @@ dev_path = {
 # 正式
 pro_path = {
     # 存储源文件的路径前缀
-    "prefix": "/opt/spacewalk/data/orgin_file/",
+    "prefix": "/data/spacewalk/origin_file/",
     # 存放parquet文件的地址
     "parquet_path": "hdfs://hadoop01:9000/spacewalk/hdfs/parquet_file/",
     # master 的地址
-    "spark_master_ip": "yarn",
-    # warehouse的位置
-    "spark_warehouse": "hdfs://spacewalk/spacewalk/hdfs/ware_house"
+    "spark_master_ip": "yarn"
 }
 LOGGING_DIR = "../logs"
 LOGGING_STDOUT = True
