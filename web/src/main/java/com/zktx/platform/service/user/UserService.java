3889e75436fe0be01c03d3a843b9a9e0f72ce0f6
sailLiao <1065500519@qq.com>
2017-04-20 13:24:45 +0800

    增加了健康险用户的登录和权限的配置

--- web/src/main/java/com/zktx/platform/service/user/UserService.java
+++ web/src/main/java/com/zktx/platform/service/user/UserService.java
@@ -7,11 +7,13 @@ import com.zktx.platform.entity.tb.User;
 public interface UserService {
 
 	User findByName(String username);
-	
+
 	String login(String username, String password);
 
 	void updateToken(Integer id, String token, Date date);
 
 	User findByToken(String token);
+
+	int findSSOAuthorityNumByUid(Integer id, String sysType) throws Exception;
 
 }
